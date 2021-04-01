19e1acf278da59c0e535ae2e075be9bcaba65717
LuisRojas99 <78233292+LuisRojas99@users.noreply.github.com>
2021-04-01 01:27:52 -0600

    Initial trianlge
    Initial Triangle for Triangle fanning for raytracing lightcast has been made, tiangle has a vertex point in the character and it updates each frame

---
+++ RayTracingGame/Assets/Scripts/ProceduralMesh.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Mesh mesh;
+    public Transform Target;
+    Vector3[] vertices;
+    int[] triangles;
+    
+    void Awake() {
+        mesh = GetComponent<MeshFilter>().mesh;
+    
+    }
+    void Start()
+    {
+        MakeMeshData();
+        
+    }
+    
+    void MakeMeshData() {
+
+        vertices = new Vector3[] { Target.transform.position, new Vector3(0,1,0), new Vector3(1,0,0)};
+        triangles = new int[] { 0, 1, 2 };
+    
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        mesh.Clear();
+       
+        vertices = new Vector3[] { Target.transform.position, new Vector3(0, 1, 0), new Vector3(1, 0, 0) };
+        triangles = new int[] { 0, 1, 2 };
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
